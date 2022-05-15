14d6fb8de9e477320d30dd4134044e3b0d294e25
Armand McQueen <armandmcqueen@gmail.com>
2022-05-15 16:26:41 -0700

    Updated magnet to add buttons

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,6 @@
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
-import {
-    container,
-    heading,
-    navLinks,
-    navLinkItem,
-    navLinkText,
-    siteTitle,
-} from './layout.module.css'
+
 
 const Layout = ({ pageTitle, children }) => {
     const data = useStaticQuery(graphql`
@@ -20,31 +13,31 @@ const Layout = ({ pageTitle, children }) => {
     }
   `)
     return (
-        <div className={container}>
+        <div className="my-0 mx-auto max-w-3xl text-center">
             <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+            <header className="">{data.site.siteMetadata.title}</header>
             <nav>
-                <ul className={navLinks}>
-                    <li className={navLinkItem}>
-                        <Link to="/" className={navLinkText}>Home</Link>
+                <ul className="">
+                    <li className="">
+                        <Link to="/" className="">Home</Link>
                     </li>
-                    <li className={navLinkItem}>
-                        <Link to="/about" className={navLinkText}>About</Link>
+                    <li className="">
+                        <Link to="/about" className="">About</Link>
                     </li>
-                    <li className={navLinkItem}>
-                        <Link to="/blog" className={navLinkText}>
+                    <li className="">
+                        <Link to="/blog" className="">
                             Blog
                         </Link>
                     </li>
-                    <li className={navLinkItem}>
-                        <Link to="/magnet" className={navLinkText}>
+                    <li className="">
+                        <Link to="/magnet" className="">
                             Magnet
                         </Link>
                     </li>
                 </ul>
             </nav>
             <main>
-                <h1 className={heading}>{pageTitle}</h1>
+                <h1 className="">{pageTitle}</h1>
                 {children}
             </main>
         </div>
--- src/pages/magnet.js
+++ src/pages/magnet.js
@@ -2,17 +2,34 @@ import * as React from "react"
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import {StaticImage} from "gatsby-plugin-image";
+import { Button } from 'antd';
+
 
 const image_src = "https://www.geekyhobbies.com/wp-content/uploads/2019/03/Rummikub-First-Play.jpg"
 
 
 const MagnetPage = () => {
+    const button_row_classes = "py-2 flex flex-row items-center justify-center"
+    const button_row_splitter_classes = "flex basis-1/2 justify-center"
+    const button_classes = "basis-1/2 rounded-md bg-blue-600 text-white font-bold hover:bg-blue-800 rounded"
     return (
         <Layout pageTitle="Magnet">
+            <h3 className="text-3xl font-bold underline">Some text</h3>
             <StaticImage
+                className="rounded-xl"
                 alt="Candidate image for labeling"
                 src={image_src}
             />
+            <div className={button_row_classes}>
+                <div className={button_row_splitter_classes}>
+                    <Button type="primary" className={button_classes} >Yes</Button>
+                </div>
+                <div className={button_row_splitter_classes}>
+                    <Button type="primary" className={button_classes}>No</Button>
+                </div>
+
+            </div>
+
         </Layout>
     )
 }
